044ed4ed756999f1e703bd15d491e63ab2394c4a
Sarbad0r <avaz.shams.2002@gmail.com>
2023-07-20 07:08:54 +0500

    uploaded

--- app/src/main/java/com/example/kotlin_jetpack/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_jetpack/MainActivity.kt
@@ -3,21 +3,24 @@ package com.example.kotlin_jetpack
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Icon
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -29,67 +32,125 @@ import com.example.kotlin_jetpack.ui.theme.Kotlin_jetpackTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        var counter: Int = 1;
         setContent {
             Column(
-                modifier = Modifier
-                    .height(height = 200.dp)
-                    .background(color = Color.Blue)
-                    .fillMaxWidth()
+                modifier = Modifier.fillMaxSize()
             ) {
-                Spacer(modifier = Modifier.height(height = 30.dp))
-                Row(
-                    horizontalArrangement = Arrangement.Center,
-                    modifier = Modifier.fillMaxWidth()
+                TopWidget()
+                CardWidget(name = "Avaz", prof = "Programmer")
+                CardWidget(name = "PopUp", prof = "Programmer")
+                CardWidget(name = "Tennis", prof = "Programmer")
+            }
+        }
+    }
+}
+
+
+@Composable
+fun TopWidget() {
+    Column(
+        modifier = Modifier
+            .height(height = 200.dp)
+            .background(color = Color(0xFF4472C4))
+            .fillMaxWidth()
+    ) {
+        Spacer(modifier = Modifier.height(height = 30.dp))
+        Row(
+            horizontalArrangement = Arrangement.Center, modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(
+                text = "Where do you want to travel", style = TextStyle(
+                    color = Color.White,
+                    fontSize = 20.sp,
+                    textAlign = TextAlign.Center,
+                    fontWeight = FontWeight.Bold
+                )
+            )
+        }
+        Spacer(modifier = Modifier.height(height = 20.dp))
+        Row(
+            modifier = Modifier.fillMaxSize(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceEvenly
+        ) {
+            Box(
+                modifier = Modifier
+                    .size(50.dp)
+                    .background(Color.White, shape = RoundedCornerShape(10.dp)),
+                contentAlignment = Alignment.Center
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Favorite, contentDescription = "Favorite Icon"
+                )
+            }
+            Box(
+                modifier = Modifier
+                    .padding(30.dp)
+                    .background(color = Color.White, shape = RoundedCornerShape(10.dp))
+            ) {
+                Text(text = "Select all stuff that you want", modifier = Modifier.padding(10.dp))
+            }
+
+            Box(
+                modifier = Modifier
+                    .size(50.dp)
+                    .background(Color.White, shape = RoundedCornerShape(10.dp)),
+                contentAlignment = Alignment.Center
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Search, contentDescription = "Favorite Icon"
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun CardWidget(name: String, prof: String?) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(10.dp),
+        shape = RoundedCornerShape(10.dp),
+        elevation = 5.dp
+    ) {
+        Box(
+            modifier = Modifier.padding(10.dp)
+        ) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.ic_launcher_background),
+                    contentDescription = "image",
+                    contentScale = ContentScale.FillHeight,
+                    modifier = Modifier
+                        .size(60.dp)
+                        .clip(CircleShape)
+                )
+                Spacer(modifier = Modifier.width(10.dp))
+                Column(
+                    modifier = Modifier.fillMaxWidth(), verticalArrangement = Arrangement.Center
                 ) {
                     Text(
-                        text = "Where do you want to travel",
-                        style = TextStyle(
-                            color = Color.White,
-                            fontSize = 20.sp,
-                            textAlign = TextAlign.Center,
-                            fontWeight = FontWeight.Bold
-                        )
+                        text = name,
+                        style = TextStyle(fontWeight = FontWeight.Bold, fontSize = 20.sp)
                     )
-                }
-                Spacer(modifier = Modifier.height(height = 20.dp))
-                Row(
-                    modifier = Modifier
-                        .fillMaxSize(),
-                    verticalAlignment = Alignment.CenterVertically,
-                    horizontalArrangement = Arrangement.SpaceEvenly
-                ) {
-                    Box(
-                        modifier = Modifier
-                            .size(50.dp)
-                            .background(Color.White, shape = RoundedCornerShape(10.dp)),
-                        contentAlignment = Alignment.Center
-                    ) {
-                        Icon(
-                            imageVector = Icons.Default.Favorite,
-                            contentDescription = "Favorite Icon"
-                        )
-                    }
-                    Box(
-                        modifier = Modifier
-                            .padding(30.dp)
-                            .background(color = Color.White, shape = RoundedCornerShape(10.dp))
-                    ) {
-                        Text(text = "Select all stuff that you want")
-                    }
-
-                    Box(
-                        modifier = Modifier
-                            .size(50.dp)
-                            .background(Color.White, shape = RoundedCornerShape(10.dp)),
-                        contentAlignment = Alignment.Center
-                    ) {
-                        Icon(
-                            imageVector = Icons.Default.Search,
-                            contentDescription = "Favorite Icon"
+                    prof?.let {
+                        Text(
+                            text = it,
+                            style = TextStyle(fontWeight = FontWeight.Bold, fontSize = 20.sp)
                         )
                     }
                 }
             }
         }
     }
+}
+
+@Preview
+@Composable
+fun ForPreview() {
+    CardWidget(name = "Avaz", prof = "Programmer")
 }
